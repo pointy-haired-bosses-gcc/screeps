8ac0c94dbd50e1d67783438a046e094678877b37
Will Killian <github.user@forkmantis.com>
2016-09-23 07:24:55 -0500

    Add prioritization to auto spawning so we always have harvesters and transporters above all else.

--- dist/main.js
+++ dist/main.js
@@ -48,27 +48,31 @@ module.exports.loop = function () {
         }
         
         var harvesters = _.filter(creepsInRoom, (creep) => creep.memory.role == 'harvester');
-        if(harvesters.length < 2) {
+        var transporters = _.sum(creepsInRoom, (creep) => creep.memory.role == 'transporter');
+        if(harvesters.length < 1) {
             var newName = spawnsInRoom[0].createCreep([WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'harvester'});
             console.log('Spawning new harvester: ' + newName);
         }
-
-        var transporters = _.sum(creepsInRoom, (creep) => creep.memory.role == 'transporter');
-        if (transporters.length < 2) {
+        else if (transporters.length < 2) {
             var newName = spawnsInRoom[0].createCrep([WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'transporter'});
             console.log('Spawning new transporter: ' + newName);
         }
-
-        var upgraders = _.sum(creepsInRoom, (creep) => creep.memory.role == 'upgrader');
-        if(upgraders < 4) {
-            var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader: ' + newName);
+        else if (harvesters.length < 2) {
+            var newName = spawnsInRoom[0].createCreep([WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'harvester'});
+            console.log('Spawning new harvester: ' + newName);
         }
+        else {
+            var upgraders = _.sum(creepsInRoom, (creep) => creep.memory.role == 'upgrader');
+            if(upgraders < 4) {
+                var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'upgrader'});
+                console.log('Spawning new upgrader: ' + newName);
+            }
 
-        var builders = _.sum(creepsInRoom, (creep) => creep.memory.role == 'builder');
-        if (builders < 2) {
-            var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'builder'});
-            console.log('Spawning new builder: ' + newName);
+            var builders = _.sum(creepsInRoom, (creep) => creep.memory.role == 'builder');
+            if (builders < 2) {
+                var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'builder'});
+                console.log('Spawning new builder: ' + newName);
+            }
         }
 
         var tower = Game.getObjectById('TOWER_ID');
