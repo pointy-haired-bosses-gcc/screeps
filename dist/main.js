d7b461ea4d39105f50557a27709a38575416754a
Will Killian <github.user@forkmantis.com>
2016-09-22 22:07:42 -0500

    Build builders always, since tyey repair.  Add 4th upgrader.

--- dist/main.js
+++ dist/main.js
@@ -60,17 +60,15 @@ module.exports.loop = function () {
         }
 
         var upgraders = _.sum(creepsInRoom, (creep) => creep.memory.role == 'upgrader');
-        if(upgraders < 3) {
+        if(upgraders < 4) {
             var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
         }
 
-        if (room.find(FIND_CONSTRUCTION_SITES).length > 0) {
-            var builders = _.sum(creepsInRoom, (creep) => creep.memory.role === 'builder');
-            if (builders.length < 2) {
-                var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'builder'});
-                console.log('Spawning new builder: ' + newName);
-            }
+        var builders = _.sum(creepsInRoom, (creep) => creep.memory.role == 'builder');
+        if (builders < 2) {
+            var newName = spawnsInRoom[0].createCreep([WORK,CARRY,MOVE,CARRY,MOVE], undefined, {role: 'builder'});
+            console.log('Spawning new builder: ' + newName);
         }
 
         var tower = Game.getObjectById('TOWER_ID');
